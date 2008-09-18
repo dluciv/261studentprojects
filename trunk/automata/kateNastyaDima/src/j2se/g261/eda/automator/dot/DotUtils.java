a73baf3a0cd8a73788f904e3c860fa0424f54017
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-09-18 18:53:19 +0000

    class for dot utils like "create new dot file" and other

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/dot/DotUtils.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package j2se.g261.eda.automator.dot;
+
+/**
+ *
+ * @author nastya
+ */
+public class DotUtils {
+
+    public static final int DETERMINATE = 0;
+    public static final int NON_DETERMINATE = 1;
+    
+    private DotUtils(){
+        
+    }
+    
+    public static void generateDotFile(String filename, int type){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    
+}
