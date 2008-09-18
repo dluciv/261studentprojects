4eb1c36481d917b3d0cc39957822c028d1e5fac1
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-09-18 18:50:01 +0000

    Class represents worker with static methods

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/graph/GraphWorker.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package j2se.g261.eda.automator.graph;
+
+/**
+ *
+ * @author nastya
+ */
+public class GraphWorker {
+    
+    
+    private GraphWorker(){
+        
+    }
+    
+    
+    //slogenie
+    public static Graph concatenateOR(Graph g1, Graph g2){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    //slogenie s 0
+    //dlya togo chto by po zadache razdelyat' dva sluchaya
+    public static Graph concatenateONE(Graph g1){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //zvzdochka Klini
+    public static Graph concatanateANY(Graph g){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //umnogenie
+    public static Graph concatanateAND(Graph g1, Graph g2){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //transitive closure
+    public static Graph makeClosure(Graph g){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //remove epsilon-nodes
+    public static Graph removeEpsilonNodes(Graph g){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //mark all start and ends nodes
+    public static Graph markAllNodes(Graph g){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
