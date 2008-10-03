4746bbff27b4b7441dea788d5b06a2ed5562cc40
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-10-03 14:15:06 +0000

    all Vectors replaced by HashSets because of all List contains methods implemented with object's equals  method and so, we have wrong result

--- trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/graph/Graph.java
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/graph/Graph.java
@@ -1,38 +1,75 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Graph.java
+ * 
+ * Version 1.0
+ * 
+ * 16.0.2008
+ * 
+ * (c) "EDA"
  */
 package j2se.g261.eda.automator.graph;
 
+import j2se.g261.eda.automator.table.Table;
+import j2se.g261.eda.automator.table.TableRecord;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Vector;
 
 /**
- *
- * @author Anastasiya, Dmitry
+ *Represents graph with start and end nodes. Created for presentation of 
+ * NFA and DFA. All actions you may perform with graph locates in GraphWorker 
+ * class. Graph object just a storage for nodes with possibility of node 
+ * ordering accordingly to topologoical role(start, end or nothing)
+ * 
+ * 
+ * @author Anastasiya
+ * @author Dmitry
+ * 
+ * @see j2se.g261.eda.automator.graph.GraphWorker
  */
-public class Graph {
+public class Graph implements Cloneable{
 
-    private Vector<Node> starts;
-    private Vector<Node> ends;
-    private Vector<Node> all;
+    /**
+     * Storage for start nodes
+     */
+    private HashSet<Node> starts;
+    /**
+     * Storage for end nodes
+     */
+    private HashSet<Node> ends;
+    /**
+     * Storage for all nodes
+     */
+    private HashSet<Node> all;
 
+    /**
+     * Constructor with one node which will be only node in graph.
+     * 
+     * @param root 
+     *        root node. This node will be only node in graph. It is start 
+     *        and end node.
+     */
     public Graph(Node root) {
-        starts = new Vector<Node>();
-        ends = new Vector<Node>();
-        all = new Vector<Node>();
+        starts = new HashSet<Node>();
+        ends = new HashSet<Node>();
+        all = new HashSet<Node>();
         starts.add(root);
         ends.add(root);
         all.add(root);
     }
 
-    public void addNode(Node n) {
+    /**
+     * Recursively adds node to graph. All connected node will be added too.
+     * 
+     * @param n node to be added to graph. 
+     */
+    void addNode(Node n) {
         if (!all.contains(n)) {
             all.add(n);
         }
 
         int num = n.getOutgoingSize();
-        
+
         for (int i = 0; i < num; i++) {
             Node n1 = n.getOutgoingAt(i);
             if (!all.contains(n1)) {
@@ -41,13 +78,30 @@ public class Graph {
         }
     }
 
-    public void deleteNode(Node n) {
+    /**
+     * Deletes node from graph with keeping of connections between nodes
+     * 
+     * @param n
+     *        node to be deleted
+     */
+    void deleteNode(Node n) {
         n.prepareForDeleting();
         all.remove(n);
         starts.remove(n);
         ends.remove(n);
     }
+    
+    void simpleDeleteNode(Node n){
+        all.remove(n);
+        starts.remove(n);
+        ends.remove(n);
+    }
 
+    /**
+     * Represents graph as string (for debug)
+     * 
+     * @return string representation of graph
+     */
     @Override
     public String toString() {
         String s = "Graph: " + all.size() + "nodes\n";
@@ -62,74 +116,187 @@ public class Graph {
         return s;
     }
 
+    /**
+     * Constructs epsilon-graph. Epsilon-graph have one node with epsilon-name.
+     * 
+     * @return epsilon-graph
+     */
     public static Graph getEmpty() {
         return new Graph(Node.getEpsilonNode());
     }
-    
 
-    public void markAllStarts() {
-        starts.removeAllElements();
+    /**
+     * 
+     * Walks through the graph and checks each node if it haven't incoming 
+     * connections. If true when marks node as start.
+     * 
+     * @see j2se.g261.eda.automator.graph.Graph#markAllEnds() 
+     */
+    void markAllStarts() {
+        starts.removeAll(starts);
 
         for (Node node : all) {
-            
+
             if (!node.haveIncoming()) {
                 starts.add(node);
             }
         }
     }
 
-    public void markAllEnds(){
-        
-        ends.removeAllElements();
-        
+    /**
+     * 
+     * Walks through the graph and checks each node if it haven't outgoing 
+     * connections. If true when marks node as end.
+     * 
+     * @see j2se.g261.eda.automator.graph.Graph#markAllStarts() 
+     * @see j2se.g261.eda.automator.graph.Graph#isEnd(j2se.g261.eda.automator.graph.Node) 
+     */
+    void markAllEnds() {
+
+        ends.removeAll(ends);
+
         for (Node node : all) {
             System.out.println(node.getOutgoingSize());
             if (!node.haveOutgoing()) {
                 ends.add(node);
             }
         }
-        
+
     }
-    
-    public boolean isStart(Node n){
+
+    /**
+     * Checks if node marked as start node
+     * 
+     * @param n node to be tested
+     * @return {@code  true} if node marked as start, {@code false} otherwise
+     * 
+     * @see j2se.g261.eda.automator.graph.Graph#markAllStarts() 
+     * @see j2se.g261.eda.automator.graph.Graph#isEnd(j2se.g261.eda.automator.graph.Node) 
+     */
+    boolean isStart(Node n) {
         return (starts.contains(n));
     }
-    
-    public int startsSize(){
+
+    /**
+     * Returns number of start nodes (size of start node list)
+     * 
+     * @return start nodes size
+     */
+    public int startsSize() {
         return starts.size();
     }
-    
-    public int endsSize(){
+
+    /**
+     * Returns number of end nodes (size of end node list)
+     * 
+     * @return end nodes size
+     */
+    int endsSize() {
         return ends.size();
     }
-    
-    public int allSize(){
+
+
+    /**
+     * Returns number of nodes ing graph(size of all node list)
+     * 
+     * @return all nodes size
+     */
+    int allSize() {
         return all.size();
     }
+
     
-    public Node getNodeFromStartsAt(int index){
-        return starts.get(index);
+    /**
+     * Returns node in specified position in start node list.
+     * 
+     * @param index index of element to return
+     * @return node at the specified index
+     */
+    
+    public Node getNodeFromStartsAt(int index) {
+        return (Node)(starts.toArray())[index];
     }
-    
-    public Node getNodeFromEndsAt(int index){
-        return ends.get(index);
+
+    /**
+     * Returns node in specified position in end node list.
+     * 
+     * @param index index of element to return
+     * @return node at the specified index
+     */
+    Node getNodeFromEndsAt(int index) {
+        return (Node)(ends.toArray())[index];
     }
-    
-    public Node getNodeFromAllAt(int index){
-        return all.get(index);
+
+
+    /**
+     * Returns node in specified position in all node list.
+     * 
+     * @param index index of element to return
+     * @return node at the specified index
+     */
+    Node getNodeFromAllAt(int index) {
+        return (Node)(all.toArray())[index];
     }
-    
-    public int getNodeIndex(Node n){
+
+    int getNodeIndex(Node n) {
         for (int i = 0; i < all.size(); i++) {
-            if(all.get(i) == n){
+            if ((Node)(all.toArray())[i] == n) {
                 return i;
             }
         }
         return -1;
     }
 
+    /**
+     * Checks if node marked as end node
+     * 
+     * @param n node to be tested
+     * @return {@code  true} if node marked as end, {@code false} otherwise
+     * 
+     * @see j2se.g261.eda.automator.graph.Graph#markAllEnds() 
+     * @see j2se.g261.eda.automator.graph.Graph#isStart(j2se.g261.eda.automator.graph.Node) 
+     */
     boolean isEnd(Node n) {
         return ends.contains(n);
     }
+    
+    //@todo rewrite accordingly to replacing; document
+    public void fillDeterminatedTable(Table table) {
+        table.clear();
+        int a = allSize();
+        for (int i = 0; i < a; i++) {
+            writeNodeInfoToTable(getNodeFromAllAt(i), table);
+        
+        }
+    }
 
+    private void writeNodeInfoToTable(Node n, Table table) {
+        TableRecord t = new TableRecord();
+        
+        if(Node.isEndNode(n)){
+            t.add(TableRecord.SYMBOL_END, 0);
+        }
+        int a = n.getOutgoingSize();
+        for (int i = 0; i < a; i++) {
+            Node n1 = n.getOutgoingAt(i);
+            t.add(n1.getName(), getNodeIndex(n1));
+        }
+        table.add(getNodeIndex(n), t);
+    }
+    
+    @Override
+    public Graph clone(){
+        return null;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        Vector<Node> v = new Vector<Node>();
+        
+        v.add(new Node('a'));
+        System.out.println(v.contains(new Node('a')));
+        
+        
+    }
 }
