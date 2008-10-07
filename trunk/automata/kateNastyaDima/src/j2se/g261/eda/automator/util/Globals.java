0563d16baf8223f14bd98cc88dcf500717180962
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-10-07 09:33:18 +0000

    Represents options like command-line arguments and utilites command for representing graph and table

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/util/Globals.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package j2se.g261.eda.automator.util;
+
+/**
+ *
+ * @author nastya
+ */
+public class Globals {
+
+    public static String LATEX_COMMAND = "latex";
+    public static String DVI_VIEWER = "kdvi";
+    public static String DOT_VIEWR = "dot";
+    
+    public static boolean COMMANDS_IN_ENV = true;
+}
