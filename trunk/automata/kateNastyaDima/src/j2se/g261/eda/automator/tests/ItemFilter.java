a81c93fcafdc6a9b6bf1a664c5818dd56298bfdf
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-10-25 22:00:43 +0000

    Represents simple filter

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/tests/ItemFilter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package j2se.g261.eda.automator.tests;
+
+/**
+ *
+ * @author nastya
+ */
+public interface ItemFilter {
+    public boolean include(TestResultItem item);
+}
