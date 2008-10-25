77112ab59d5c91a8a325fdefb90f3c72c6ded5d5
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-10-25 22:02:07 +0000

    Represents simple couple of file with tests and it's tests(deserialized content) or it's test's results

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/tests/TestFile.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package j2se.g261.eda.automator.tests;
+
+import java.io.File;
+
+/**
+ *
+ * @author nastya
+ */
+public class TestFile<ST> implements Comparable<TestFile>{
+    File file;
+    ST storage;
+
+    public TestFile(File file, ST storage) {
+        this.file = file;
+        this.storage = storage;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public ST getStorage() {
+        return storage;
+    }
+
+    public void setStorage(ST storage) {
+        this.storage = storage;
+    }
+
+    public int compareTo(TestFile o) {
+        return file.compareTo(o.getFile());
+    }
+    
+    
+}
