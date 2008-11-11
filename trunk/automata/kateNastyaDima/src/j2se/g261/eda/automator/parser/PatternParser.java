616c1f84521d7240fa8f6a0d6cd8e9735a01d3bf
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-11-11 00:20:02 +0000

    Commented byaka

--- trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/parser/PatternParser.java
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/parser/PatternParser.java
@@ -5,22 +5,9 @@
 
 package j2se.g261.eda.automator.parser;
 
-import j2se.g261.eda.automator.graph.Graph;
-import j2se.g261.eda.automator.graph.GraphWalker;
-import j2se.g261.eda.automator.graph.GraphWorker;
-import j2se.g261.eda.automator.graph.Node;
-import j2se.g261.eda.automator.graph.WalkerException;
-import j2se.g261.eda.automator.table.Table;
-import j2se.g261.eda.automator.table.TableWalker;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.io.IOException;
-import j2se.g261.eda.automator.dot.DotException;
-import j2se.g261.eda.automator.dot.DotUtils;
-import java.util.HashMap;
-import j2se.g261.eda.automator.minimization.Minimisation;
-import j2se.g261.eda.automator.minimization.TransformToEdge;
-import j2se.g261.eda.automator.minimization.MinGraph;
+import j2se.g261.eda.automator.representations.nfa.NFA;
+import j2se.g261.eda.automator.representations.nfa.NFAWorker;
+import j2se.g261.eda.automator.representations.nfa.NFANode;
 import j2se.g261.eda.automator.minimization.EdgeGraphWolker;
 
 /**
@@ -36,16 +23,16 @@ public class PatternParser {
     }
 
     
-    public Graph parse() throws ParserException{
-        Graph g = expression();
+    public NFA parse() throws ParserException{
+        NFA g = expression();
         if(!lexer.isEndOfLine())
             throw new ParserException(ParserException.WRONG_USING_OPERATORS);
         return g;
         
     }
     
-    private Graph atom() throws ParserException {
-            Graph res = null;
+    private NFA atom() throws ParserException {
+            NFA res = null;
         char nextLexem = lexer.nextChar();
         if(nextLexem == '('){
             res = expression();
@@ -55,38 +42,38 @@ public class PatternParser {
             return res;
         }else if(isSymbol(nextLexem)){
             
-            return new Graph(new Node(nextLexem));
+            return new NFA(new NFANode(nextLexem));
         }else{
             throw new ParserException(ParserException.WRONG_USING_OPERATORS);
         }
     }
     
-    private Graph expression() throws ParserException{
+    private NFA expression() throws ParserException{
         
-        Graph res = sequence();
+        NFA res = sequence();
         if(lexer.isEndOfLine()) return res;
         while(!lexer.isEndOfLine() && lexer.nextChar() == '|'){
-            res = GraphWorker.concatenateOR(res, sequence());
+            res = NFAWorker.concatenateOR(res, sequence());
         }
         return res;
     }
 
-    private Graph post_option() throws ParserException {
-        Graph res = atom();
+    private NFA post_option() throws ParserException {
+        NFA res = atom();
         if(lexer.isEndOfLine()) return res;
         char nextChar = lexer.nextChar();
         if(nextChar == '*'){
-            return GraphWorker.concatanateANY(res);
+            return NFAWorker.concatanateANY(res);
         }else if(nextChar == '?'){
-            return GraphWorker.concatenateONE(res);
+            return NFAWorker.concatenateONE(res);
         }else{
             lexer.decrementCursor();
             return res;
         }
     }
 
-    private Graph sequence() throws ParserException {
-        Graph res = post_option();
+    private NFA sequence() throws ParserException {
+        NFA res = post_option();
 //        System.out.println("post option \n" + res);
         
         if(lexer.isEndOfLine()) return res;
@@ -97,9 +84,9 @@ public class PatternParser {
                 lexer.decrementCursor();
                 return res;
             }
-            Graph nw = post_option();
+            NFA nw = post_option();
 //            System.out.println("new \n" + nw);
-            res = GraphWorker.concatanateAND(res, nw);
+            res = NFAWorker.concatanateAND(res, nw);
             if(lexer.isEndOfLine()) return res;
             nextChar = lexer.nextChar();
             lexer.decrementCursor();
@@ -112,65 +99,59 @@ public class PatternParser {
     private boolean isSymbol(char c){
         return c != '|' && c != '*' && c != '?';
     }
-    
-    public static void main(String[] args) {
-        
-        PatternParser p = new PatternParser("(0|1)*");
-        int c;
-        try {
-            Graph g = p.parse();
-            Table t = new Table();
-            DotUtils d1 = new DotUtils(g);
-            try {
-//              System.out.println(d.generateDotFileForNFA("DOTFILE").getAbsolutePath());
-              System.out.println(d1.generateDotFileForNFA("NFA").getAbsolutePath());
-          } catch (IOException ex) {
-              Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
-          } catch (DotException ex) {
-              Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
-          }
-
-            GraphWorker.makeClosure(g);
-            Graph g1 = GraphWorker.makeDeterministic(g);
-
-            g.fillDeterminatedTable(t);
-            t.fillTable();
-            
-//            DotUtils d = new DotUtils(g);
-          
-          /*  System.out.println(g1);
-            System.out.println("!!!!!!!!!!");
-            for (int i = 0; i < g1.allSize(); i++) {
-                System.out.println(g1.getNodeFromAllAt(i));
-            }*/
-            
-            TransformToEdge tte = new TransformToEdge();
-            MinGraph g2 = tte.transform(g1);
-            try{
-            	System.out.println(d1.edgeDot("DFA",g2).getAbsolutePath());
-            } catch (IOException ex){
-            	Logger.getLogger(Minimisation.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Minimisation m1 = new Minimisation(g2);
-            MinGraph g3 = m1.minimize();
-            EdgeGraphWolker egw = new EdgeGraphWolker(g3);
-            System.out.println(egw.check("101010101"));
-            try{
-            	System.out.println(d1.edgeDot("EDGEGRAPH",g2).getAbsolutePath());
-            } catch (IOException ex){
-            	Logger.getLogger(Minimisation.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            	
-            GraphWalker walker = new GraphWalker(g);
-            System.out.println(walker.check("a"));
-           //TableWalker walk = new TableWalker(g,t);
-           // System.out.println(walk.check(""));
-            
-
-        } catch (WalkerException ex) {
-            Logger.getLogger(PatternParser.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParserException ex) {
-            Logger.getLogger(PatternParser.class.getName()).log(Level.SEVERE, null, ex);
-        }
-     }
+//
+//    public static void main(String[] args) {
+//
+//        PatternParser p = new PatternParser("(1000|1010)(0|1)*");
+//        int c;
+//        try {
+//            NFA g = p.parse();
+//            Table t = new Table();
+//            NFAWorker.makeClosure(g);
+////            NFA g1 = NFAWorker.makeDeterministic(g);
+//
+//            g.fillDeterminatedTable(t);
+//            t.fillTable();
+//
+////            DotUtils d = new DotUtils(g);
+//            DotUtils d1 = new DotUtils(g);
+//          /*  System.out.println(g1);
+//            System.out.println("!!!!!!!!!!");
+//            for (int i = 0; i < g1.allSize(); i++) {
+//                System.out.println(g1.getNodeFromAllAt(i));
+//            }*/
+//
+//            TransformToEdge tte = new TransformToEdge();
+//            MinNFA g2 = tte.transform(g1);
+//            Minimisation m1 = new Minimisation(g2);
+//            m1.minimize();
+//            System.out.println(m1.check("101010101", g2));
+//            try{
+//            	System.out.println(d1.edgeDot("DOTTry",g2).getAbsolutePath());
+//            } catch (IOException ex){
+//            	Logger.getLogger(Minimisation.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//
+//          /*  try {
+////              System.out.println(d.generateDotFileForNFA("DOTFILE").getAbsolutePath());
+//              System.out.println(d1.generateDotFileForNFA("DOTFILE").getAbsolutePath());
+//          } catch (IOException ex) {
+//              Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
+//          } catch (DotException ex) {
+//              Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
+//          }*/
+//
+//
+//            NFAWalker walker = new NFAWalker(g);
+//            System.out.println(walker.check("a"));
+//           //TableWalker walk = new TableWalker(g,t);
+//           // System.out.println(walk.check(""));
+//
+//
+//        } catch (WalkerException ex) {
+//            Logger.getLogger(PatternParser.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (ParserException ex) {
+//            Logger.getLogger(PatternParser.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//     }
 }
