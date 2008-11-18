d3ffe7643a04eb45be3331def6df9ac93b612f47
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-11-18 10:49:38 +0000

    It seems something work O_o

---
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/testsmaker/table/ColumnData.java
@@ -0,0 +1,19 @@
+package j2se.g261.eda.testsmaker.table;
+
+/**
+ *Represents column data used in table
+ * 
+ * @author Anastasiya
+ */
+public class ColumnData {
+
+    public String title;
+    public int width;
+    public int alignment;
+
+    public ColumnData(String title, int width, int alignment) {
+        this.title = title;
+        this.width = width;
+        this.alignment = alignment;
+    }
+}
