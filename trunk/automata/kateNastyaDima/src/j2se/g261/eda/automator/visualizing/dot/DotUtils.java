27ac8e61160d9c219faabf640b2bb088ca31efb7
A.S.Solodkaya <A.S.Solodkaya@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-11-18 10:49:22 +0000

    It seems something work O_o

--- trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/visualizing/dot/DotUtils.java
+++ trunk/automata/kateNastyaDima/src/j2se/g261/eda/automator/visualizing/dot/DotUtils.java
@@ -25,15 +25,11 @@ import java.util.HashMap;
 public class DotUtils {
 
     private static final String DIGRAPH = "digraph";
-    private NFA nfa;
-    private DFA dfa;
-    private MinimizedDFA mdfa; 
-    private int index = 0;
+    private static final String EXTENSION = ".dot";
+    private int index;
 
-    public DotUtils(NFA n, DFA d, MinimizedDFA m) {
-        this.nfa = n;
-        this.dfa = d;
-        this.mdfa = m;
+    public DotUtils() {
+        index = 0;
     }
 
     /**
@@ -44,10 +40,10 @@ public class DotUtils {
      * @throws IOException if some IO errors occured
      * @throws DotException if some internal errors occured
      */
-    public File generateDotFileForNFA(String filename) throws IOException, DotException {
+    public File generateDotFileForNFA(NFA nfa, String filename) throws IOException, DotException {
         index = 0;
         HashMap<NFANode, String> passed = new HashMap<NFANode, String>();
-        File f1 = File.createTempFile(filename, ".dot");
+        File f1 = File.createTempFile(filename, EXTENSION);
 //        f1.deleteOnExit();
         BufferedWriter bf = new BufferedWriter(new FileWriter(f1));
         bf.write(DIGRAPH);
@@ -61,35 +57,35 @@ public class DotUtils {
         bf.close();
         return f1;
     }
-    
-    public File generateDotFileForDFA(String filename) throws IOException, DotException {
-    	File f1 = File.createTempFile(filename, ".dot");
-    	MinimizedDFAWorker mdw = new MinimizedDFAWorker();
-    	MinimizedDFA h = mdw.convertFromNFAToMinimizedDFA(dfa);
+   
+    public File generateDotFileForDFA(DFA dfa, String filename) throws IOException, DotException {
+        File f1 = File.createTempFile(filename, EXTENSION);
+        MinimizedDFAWorker mdw = new MinimizedDFAWorker();
+        MinimizedDFA h = mdw.convertFromNFAToMinimizedDFA(dfa);
         BufferedWriter bf = new BufferedWriter(new FileWriter(f1));
-        bf.write("digraph");
+        bf.write(DIGRAPH);
         bf.write(" g{");
         bf.newLine();
-        
+       
         int num = h.sizeAll();
         for(int i = 0; i<num ;i++){
-        	Edge edge = h.getEdgeAt(i); 
-        	if(edge.getName() == '\n'){
-        		bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
-            			"end" + "];");
-            	bf.newLine();
-        	}
-        	else{
-        	bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
-        			edge.getName() + "];");
-        	bf.newLine();
-        	}
+                Edge edge = h.getEdgeAt(i);
+                if(edge.getName() == '\n'){
+                        bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
+                                "end" + "];");
+                bf.newLine();
+                }
+                else{
+                bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
+                                edge.getName() + "];");
+                bf.newLine();
+                }
         }
         bf.write("}");
         bf.close();
-        return f1;	
+        return f1;      
     }
-    
+   
     /**
      * This method writes down node in a file
      * The starts nodes have a rectangle form, the end nodes have circle form.
@@ -98,24 +94,24 @@ public class DotUtils {
      * @return "name" of node
      * @throws IOException
      */
-    
+   
     private String writeNode(NFANode n, BufferedWriter bf) throws IOException {
         String s1;
         if (n.getIncomingSize() == 0){
-        	s1 = "node" + index + "[label = \"" + "START" + "\" ";
+                s1 = "node" + index + "[label = \"" + "START" + "\" ";
             s1 += ", shape = \"rectangle\"";
         }
         else if (n.getOutgoingSize() == 0) {
-        	s1 = "node" + index + "[label = \"" + "END" + "\" ";
+                s1 = "node" + index + "[label = \"" + "END" + "\" ";
             s1 += ", shape = \"rectangle\"";
         }
         else if (n.getName() == '\r'){
-        	s1 = "node" + index + "[label = \"" + "EPSILON" + "\" ";
+                s1 = "node" + index + "[label = \"" + "EPSILON" + "\" ";
             s1 += ", shape = \"triangle\"";
         }
         else {
-        	s1 = "node" + index + "[label = \"" + n.getName() + n.getNumber() + "\" ";
-        	s1 = "node" + index + "[label = \"" + n.getName() + "\" ";
+                s1 = "node" + index + "[label = \"" + n.getName() + n.getNumber() + "\" ";
+                s1 = "node" + index + "[label = \"" + n.getName() + "\" ";
             s1 += ", shape = \"circle\"";
         }
         bf.write(s1 + "];");
@@ -132,7 +128,7 @@ public class DotUtils {
      * @param bf the buffering
      * @throws IOException
      */
-    
+   
     private void writeEdge(String n1, String n2, BufferedWriter bf) throws IOException {
         bf.write(n1 + "->" + n2 + ";");
         bf.newLine();
@@ -146,9 +142,8 @@ public class DotUtils {
      * @throws IOException
      * @throws DotException
      */
-    
+   
     private void processNode(NFANode n, BufferedWriter bf, HashMap<NFANode, String> passed) throws IOException, DotException {
-        System.out.println("" + n.getName() + n.getNumber());
         if (!passed.containsKey(n)) {
             passed.put(n, writeNode(n, bf));
         }
@@ -159,8 +154,8 @@ public class DotUtils {
             NFANode n1 = (NFANode)n.getOutgoingAt(i);
             if (passed.containsKey(n1) && !NFANode.isEndNode(n1))
             {
-            	writeEdge(passed.get(n), passed.get(n1), bf);
-            	continue;
+                writeEdge(passed.get(n), passed.get(n1), bf);
+                continue;
             }
             processNode(n1, bf, passed);
             if (passed.containsKey(n) && passed.containsKey(n1)) {
@@ -171,7 +166,7 @@ public class DotUtils {
             }
         }
     }
-    
+   
    /**
      * This method create a dot file(In a correct format) the containing count
      * conclude file in BufferedWriter
@@ -181,106 +176,33 @@ public class DotUtils {
      * @return Temp file with dot-representation of graph
      * @throws IOException if some IO errors occured
      */
-    public File edgeDot(String filename)throws IOException{
-		File f1 = File.createTempFile(filename, ".dot");
+    public File edgeDot(MinimizedDFA mdfa, String filename)throws IOException{
+                File f1 = File.createTempFile(filename, EXTENSION);
         BufferedWriter bf = new BufferedWriter(new FileWriter(f1));
-        bf.write("digraph");
+        bf.write(DIGRAPH);
         bf.write(" g{");
         bf.newLine();
-        
+       
         int num = mdfa.sizeAll();
         for(int i = 0; i<num ;i++){
-        	Edge edge = mdfa.getEdgeAt(i); 
-        	if(edge.getName() == '\n'){
-        		bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
-            			"end" + "];");
-            	bf.newLine();
-        	}
-        	else{
-        	bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
-        			edge.getName() + "];");
-        	bf.newLine();
-        	}
+                Edge edge = mdfa.getEdgeAt(i);
+                if(edge.getName() == '\n'){
+                        bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
+                                "end" + "];");
+                bf.newLine();
+                }
+                else{
+                bf.write(edge.getIncoming() + "->" + edge.getOutgoing() + "[label = " +
+                                edge.getName() + "];");
+                bf.newLine();
+                }
         }
         bf.write("}");
         bf.close();
         return f1;
         }
-    
-    
+   
+   
 
-//    public static void main(String[] args) {
-////        NFA g1 = new NFA();
-////        g1.addNode(new NFANode('a'));
-////        GraphWorker.markAllNodes(g1);
-////        NFA g2 = new NFA();
-////        g2.addNode(new NFANode('b'));
-////        GraphWorker.markAllNodes(g2);
-////        NFA g3 = GraphWorker.concatanateAND(g1, g2);
-////        System.out.println("______________G3______________");
-////        System.out.println(g3);
-////        System.out.println("______________________________");
-////
-////        g1 = new NFA();
-////        g2 = new NFA();
-////        g1.addNode(new NFANode('c'));
-////        GraphWorker.markAllNodes(g1);
-////        g2.addNode(new NFANode('d'));
-////        GraphWorker.markAllNodes(g2);
-////        NFA g4 = GraphWorker.concatanateANY(g1);
-////        System.out.println("______________G4______________");
-////        System.out.println(g4);
-////        System.out.println("______________________________");
-////        NFA g5 = GraphWorker.concatenateONE(g2);
-////        System.out.println("______________G5______________");
-////        System.out.println(g5);
-////        System.out.println("______________________________");
-////        NFA g0 = GraphWorker.concatanateAND(g4, g5);
-////        System.out.println("______________G0______________");
-////        System.out.println(g0);
-////        System.out.println("______________________________");
-////        NFA g6 = GraphWorker.concatenateOR(g3, g0);
-////        System.out.println("______________G6______________");
-////        System.out.println(g6);
-////        System.out.println("______________________________");
-////        GraphWorker.makeClosure(g6);
-////        System.out.println(g6);
-
-//
-//        g1 = new NFA();
-//        g2 = new NFA();
-//        g1.addNode(new NFANode('c'));
-//        GraphWorker.markAllNodes(g1);
-//        g2.addNode(new NFANode('d'));
-//        GraphWorker.markAllNodes(g2);
-//        NFA g4 = GraphWorker.concatanateANY(g1);
-//        System.out.println("______________G4______________");
-//        System.out.println(g4);
-//        System.out.println("______________________________");
-//        NFA g5 = GraphWorker.concatenateONE(g2);
-//        System.out.println("______________G5______________");
-//        System.out.println(g5);
-//        System.out.println("______________________________");
-//        NFA g0 = GraphWorker.concatanateAND(g4, g5);
-//        System.out.println("______________G0______________");
-//        System.out.println(g0);
-//        System.out.println("______________________________");
-//        NFA g6 = GraphWorker.concatenateOR(g3, g0);
-//        System.out.println("______________G6______________");
-//        System.out.println(g6);
-//        System.out.println("______________________________");
-//        GraphWorker.makeClosure(g6);
-//        System.out.println(g6);
-
-//        System.out.println(g6);
-//        DotUtils d = new DotUtils(g6);
-//        try {
-//            d.generateDotFileForNFA("DOTFILE");
-//        } catch (IOException ex) {
-//            Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (DotException ex) {
-//            Logger.getLogger(DotUtils.class.getName()).log(Level.SEVERE, null, ex);
-//        }
     }
-
 
