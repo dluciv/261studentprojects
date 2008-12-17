b114fbd22b679d0559d84ff810510264923f0f43
ksenyiacypan <ksenyiacypan@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-12-17 10:32:23 +0000

    set Rules for unnar adding, set Rules.run()

--- trunk/turingmachine/ArtemXeniaSerg/art779/turingmachine/main/Performer.java
+++ trunk/turingmachine/ArtemXeniaSerg/art779/turingmachine/main/Performer.java
@@ -4,7 +4,43 @@ public class Performer {
 
 	public void run()
 	{
-		// тут должна испольняться МТ
+		Tape tape = new Tape();
+		Rules rules = new Rules();
+		tape.printTape();
+
+		String theState = Tape.startSym;
+		String theSym = tape.curSym();
+
+		/*
+		System.out.println(rules.toString());		
+		RuleSet toDo = rules.getRule(theState, theSym);
+		System.out.println(toDo.toString());
+		*/
+
+		int k=0;
+		while(theState!=Tape.finalSym && k<99)
+		{
+			k++;
+			System.out.print(theState+theSym);
+			RuleSet toDo = rules.getRule(theState, theSym);
+			System.out.println(toDo.toString());
+			theState = toDo.getState();
+
+			String op = toDo.getOperation();
+			if(op == Rules.R)
+				tape.movePointerRight();
+			else if(op == Rules.L)
+				tape.movePointerLeft();
+			else if (op == Rules.W)
+				tape.setValue(toDo.getValue());
+			else
+			{
+				System.out.println("else");
+				break;
+			}
+			theSym = tape.curSym();
+		}
+		tape.printTape();
 
 	}
 	
--- trunk/turingmachine/ArtemXeniaSerg/art779/turingmachine/main/Rules.java
+++ trunk/turingmachine/ArtemXeniaSerg/art779/turingmachine/main/Rules.java
@@ -14,11 +14,87 @@ public class Rules {
 	{
 		RuleSet k;
 		RuleSet v;
-		
+
+		k = new RuleSet(Tape.startSym,"1");
+		v = new RuleSet("mr","W","#");
+		rules.put(k, v);
+
+		k = new RuleSet(Tape.startSym,"+");
+		v = new RuleSet(Tape.startSym,"W","#");
+		rules.put(k, v);
+
+		k = new RuleSet(Tape.startSym,"#");
+		v = new RuleSet(Tape.startSym,R);
+		rules.put(k, v);
+
+		k = new RuleSet("mr","1");
+		v = new RuleSet("mr",R);
+		rules.put(k, v);
+
+		k = new RuleSet("mr","+");
+		v = new RuleSet("mr",R);
+		rules.put(k, v);
+
+		k = new RuleSet("mr","#");
+		v = new RuleSet("mr",R);
+		rules.put(k, v);
+
+		k = new RuleSet("mr",Tape.infinitySym);
+		v = new RuleSet("=",W,"=");
+		rules.put(k, v);
+
+		k = new RuleSet("=","=");
+		v = new RuleSet("=",R);
+		rules.put(k, v);
+
+		k = new RuleSet("=",Tape.infinitySym);
+		v = new RuleSet("mle",W,"1");
+		rules.put(k, v);
+
+		k = new RuleSet("=","1");
+		v = new RuleSet("=",R);
+		rules.put(k, v);
+
+		k = new RuleSet("mle","1");
+		v = new RuleSet("mle",L);
+		rules.put(k, v);
+
+		k = new RuleSet("mle","=");
+		v = new RuleSet("mlc",L);
+		rules.put(k, v);
+
+		k = new RuleSet("mlc","1");
+		v = new RuleSet("ml",L);
+		rules.put(k, v);
+
+		k = new RuleSet("mlc","#");
+		v = new RuleSet(Tape.finalSym,H);
+		rules.put(k, v);
+
+		k = new RuleSet("ml","1");
+		v = new RuleSet("ml",L);
+		rules.put(k, v);
+
+		k = new RuleSet("ml","+");
+		v = new RuleSet("ml",L);
+		rules.put(k, v);
+
+		k = new RuleSet("ml","#");
+		v = new RuleSet(Tape.startSym,R);
+		rules.put(k, v);
+
+		k = new RuleSet("mr","=");
+		v = new RuleSet("=",R);
+		rules.put(k, v);
 	}
 	
 	public RuleSet getRule(String State, String Sym)
 	{
+		for (RuleSet vt : rules.keySet()) {
+			if(vt.getState().equals(State))
+				if(vt.getSym().equals(Sym))
+					return rules.get(vt);
+		}
 		return null;
 	}
 	
