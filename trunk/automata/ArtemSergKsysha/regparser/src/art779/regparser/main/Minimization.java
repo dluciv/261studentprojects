d8445a4f7e533c17fbbbe9f36bc24fa409745c4b
HarpSerg <HarpSerg@27a565e4-e655-0410-a04c-dd7a8689bede>
2008-12-23 12:33:34 +0000

    минимизация в новом классе

--- trunk/automata/ArtemSergKsysha/regparser/src/art779/regparser/main/Minimization.java
+++ trunk/automata/ArtemSergKsysha/regparser/src/art779/regparser/main/Minimization.java
@@ -1,321 +1,20 @@
 package art779.regparser.main;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
-public class Graph {
-    public static final int FIRST_STATE = 0;
-    public static final int LAST_STATE = 1;
-    public HashMap<Integer, String> alfabett = new HashMap<Integer, String>();
-	public HashMap<Integer, ArrayList<Integer>> graph = new HashMap<Integer, ArrayList<Integer>>();
-	private Lexer lexer;
-	public int nextStateIndex;
-	
-	Graph(){
-		alfabett.put(Graph.FIRST_STATE, "S");
-		alfabett.put(Graph.LAST_STATE, "F");
-		nextStateIndex = getlStateIndex()+1;
-	}
-	
-	public void addOneStrict(Integer node, Integer cur) {
-		ArrayList<Integer> list;
-		if(graph.containsKey(node))
-		{
-			list = graph.get(node);
-		}
-		else
-		{
-			list = new ArrayList<Integer>();
-		}		
+public class Minimization {
+	public Graph DFA;
+	Minimization(Graph TMDFA){
+		DFA = TMDFA;
 		
-		if(!list.contains(cur))
-			list.add(cur);
-		graph.put(node, list);
-		
-	}
-    
-	public void mergeSequence(Graph mergeWith)
-	{
-		alfabett.putAll(mergeWith.alfabett);
-		HashMap<Integer, ArrayList<Integer>> A = graph;
-		HashMap<Integer, ArrayList<Integer>> B = mergeWith.graph;
-		ArrayList<Integer> endsA = new ArrayList<Integer>();
-		for (Integer key : A.keySet())
-		{
-			ArrayList<Integer> val = A.get(key);
-			if(val.contains(LAST_STATE))endsA.add(key);
-		}
-		ArrayList<Integer> startsB = B.get(FIRST_STATE);
-		B.remove(FIRST_STATE);
 
-		for (int i = 0; i < endsA.size(); i++) {
-			Integer curN = endsA.get(i);
-			ArrayList<Integer> curL= A.get(curN);
-			A.remove(curN);
-			curL.remove(curL.indexOf(LAST_STATE));
-			curL.addAll(startsB);
-			for (int j = 0; j < curL.size(); j++) {
-				addOneStrict(endsA.get(i), curL.get(j));
-			}
-		}
-		A.putAll(B);		
-	}
-
-	public void mergeParallel(Graph mergeWith)
-	{
-		alfabett.putAll(mergeWith.alfabett);
 		
-		HashMap<Integer, ArrayList<Integer>> A = graph;
-		HashMap<Integer, ArrayList<Integer>> B = mergeWith.graph;
-
-		ArrayList<Integer> startsB = B.get(FIRST_STATE);
-		B.remove(FIRST_STATE);
-		
-		ArrayList<Integer> startsA = A.get(FIRST_STATE);
-		A.remove(FIRST_STATE);
-		
-		for (int i = 0; i < startsB.size(); i++) {
-			startsA.add(startsB.get(i));
-		}
-		A.put(FIRST_STATE,startsA);
-		A.putAll(B);
 	}
-
-	public void buildNode(Lexer currentLexem) {		//a
-		int pointer = currentLexem.getPointer();
-		char cur = currentLexem.getCurrent().val;	
-		int stateNum = pointer + getlStateIndex();
-
-		alfabett.put(stateNum, ""+cur);
-		addOneStrict(FIRST_STATE,stateNum);
-		addOneStrict(stateNum,LAST_STATE);
-	}
-	
-	public void buildVopros() {				//a?
-		addOneStrict(FIRST_STATE,LAST_STATE);
-	}
-	
-	public void buildZvezda() {				//a*
-		buildVopros();
-		HashMap<Integer, ArrayList<Integer>> A = graph;
-		ArrayList<Integer> endsA = new ArrayList<Integer>();
-		for (Integer key : A.keySet())
-		{
-			ArrayList<Integer> val = A.get(key);
-			if(val.contains(LAST_STATE) && key != FIRST_STATE)endsA.add(key);
-		}
-		ArrayList<Integer> startsA = A.get(FIRST_STATE);	
-		for (int i = 0; i < endsA.size(); i++) {
-			ArrayList<Integer> list = A.get(endsA.get(i));
-			A.remove(endsA.get(i));
-			list.remove(list.indexOf(LAST_STATE));
-			list.addAll(startsA);
-			A.put(endsA.get(i), list);
-		}
-	}
-	
-	public char getChar(int State) {
-		return alfabett.get(State).charAt(0);
-	}
-	
-	public int getFinalState() {
-		return LAST_STATE;
-	}
-
-	public int getlStateIndex() {
-		int preStateCount = FIRST_STATE + LAST_STATE;
-		if(preStateCount<0) preStateCount = preStateCount*(-1);
-		preStateCount++;
-		return preStateCount;
-	}	
-	
-	public ArrayList<Integer> getNextState(int State, char ch) {
-		
-		List<Integer> nStatesList = getNextState(State);
-		
-		ArrayList<Integer> nStatesListCh = new ArrayList<Integer>(); 
-		if(nStatesList == null)
-		{
-			return nStatesListCh;
-		}
-		
-		for (int i = 0; i < nStatesList.size(); i++) {
-			if(null!=alfabett.get(nStatesList.get(i)))
-			{
-				char curChar = alfabett.get(nStatesList.get(i)).charAt(0);
-				if(curChar == ch)
-				{			
-					nStatesListCh.add(nStatesList.get(i));
-				}
-			}
-		}
-		return nStatesListCh;
-	}
-	
-	public ArrayList<Integer> getNextState(int State) {
-		return graph.get(State);
-	}
-
-	public int getStartState() {
-		return FIRST_STATE;
-	}
-
-	public void addNode(Integer node, List<Integer> list) {
-		for (int k = 0; k < list.size(); k++)
-			addOne(node,list.get(k));
-	}
-
-	public void addOne(int from, int what) {
-		ArrayList<Integer> fromStates;
-		if(graph.containsKey(from))
-		{
-			fromStates = getNextState(from);
-			graph.remove(from);
-			fromStates.add(what);
-		}
-		else
-		{
-			fromStates = new ArrayList<Integer> ();
-			fromStates.add(what);
-		}
-		graph.put(from, fromStates);
-	}
-	
-	public void removeNode(int node) {
-		if(graph.containsKey(node))
-		{
-			graph.remove(node);
-		}
-	}
-	
-	public void removeOne(int from, int what) {
-		if(graph.containsKey(from))
-		{
-			List<Integer> fromStates = getNextState(from);
-			graph.remove(from);
-			fromStates.remove(fromStates.indexOf(what));
-			addNode(from, fromStates);
-		}
-	}
-
-	public void removeFromAlfabett(int node) {
-		if(alfabett.containsKey(node))
-		{
-			alfabett.remove(node);
-		}
-	}
-	
-	public boolean isInCycle(int theNode,int curKid,int checkedKid) {
-		
-		ArrayList<Integer> nStates = getNextState(curKid);
-		
-		if(nStates != null){
-			if(nStates.get(0) == null)
-				return false;
-			else if(nStates.get(0) == Graph.LAST_STATE)
-				return false;
-		}else return false;
-		
-		for (Integer curNode : nStates) {
-				if(curNode==theNode)
-				{
-					System.out.println("cycle found at " + alfabett.get(curNode)+curNode);
-					return true;
-				}
-				else{
-					if(checkedKid <= curNode){
-						return isInCycle(theNode, curNode, curNode);
-					}
-				}
-			
-		}
-		return false;
-	}
-
-	public boolean determinateNode(int curState, String val) {
-		boolean result = false;
-		
-		ArrayList<Integer> nStates = getNextState(curState,val.charAt(0));
-		
-		if(nStates.size()>1)
-		{
-			ArrayList<Integer> nnStates = new ArrayList<Integer> ();
-			for (Integer nsval : nStates) {
-				nnStates.addAll(getNextState(nsval));
-				removeOne(curState, nsval);
-				if(!isInCycle(nsval,nsval,0)){
-					System.out.println("removed node " + alfabett.get(nsval) + nsval);
-					removeNode(nsval);
-					removeFromAlfabett(nsval);
-					result = true;
-				}
-			}
-			
-			int newNodeNum = lexer.getPointer();
-			alfabett.put(newNodeNum, val);
-			System.out.println("added node " + val + newNodeNum);
-			addNode(newNodeNum, nnStates);
-			addOne(curState, newNodeNum);
-			lexer.incPointer();
-		}
-		return result;
-	}
-	
-	public void determinateNFA(Lexer lex) {
-		lexer = lex;
-		determinateBranches(getlStateIndex());
-	}
-
-	public void determinateBranches(int index) {
-		// *** как получить не ссылки, а копию ?
-		Set<Integer> allNodes = alfabett.keySet();
-		boolean modified = false;
-		
-		for (int node : allNodes) {
-			if(node >= index){
-				ArrayList<Integer> nodeKids = getNextState(node);
-				for (Integer nodeKid : nodeKids) {
-					if(determinateNode(node, alfabett.get(nodeKid))){
-						System.out.println(node + " HAS BEEN DETERMINATED");
-						index = node;
-						modified = true;
-						break;
-					}
-				}
-			}
-			if(modified)
-				break;
-		}
-		if(modified)
-			determinateBranches(index);
-	}
-	
-	public Graph buildNFA(String regexp) {
-		Parser parser = new Parser(regexp);
-		Graph NFA = parser.getNFA();
-		return NFA;
-	}
-	
-	public Graph buildDFA(String regexp) {
-		Parser parser = new Parser(regexp);
-		Graph NFA = parser.getNFA();
-		Lexer lexer = parser.getLexer();
-		NFA.determinateNFA(lexer);
-		return NFA;
-	}
-	public Graph buildDFAM(String regexp) {
-		Parser parser = new Parser(regexp);
-		Graph NFA = parser.getNFA();
-		Lexer lexer = parser.getLexer();
-		NFA.determinateNFA(lexer);
-		Minimization m = new Minimization(NFA);
-		NFA = m.minimizeDFA();		
-		return NFA;
-	}
-	/*
-	//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 	//////MINIMIZING//////MINIMIZING//////MINIMIZING//////MINIMIZING//////MINIMIZING//////
 	//////////////////////////////////////////////////////////////////////////////////////	
 	public HashMap<Integer, ArrayList<Integer>> revgraph = new HashMap<Integer, ArrayList<Integer>>();
@@ -330,7 +29,7 @@ public class Graph {
 	
 	public ArrayList<Integer> getNextSt(int State, char ch) {
 		
-		List<Integer> nStatesList = getNextState(State);
+		List<Integer> nStatesList = DFA.getNextState(State);
 		
 		ArrayList<Integer> nStatesListCh = new ArrayList<Integer>(); 
 		if(nStatesList == null)
@@ -339,9 +38,9 @@ public class Graph {
 		}
 		
 		for (int i = 0; i < nStatesList.size(); i++) {
-			if(null!=alfabett.get(nStatesList.get(i)))
+			if(null!=DFA.alfabett.get(nStatesList.get(i)))
 			{
-				char curChar = alfabett.get(nStatesList.get(i)).charAt(0);
+				char curChar = DFA.alfabett.get(nStatesList.get(i)).charAt(0);
 				if(curChar == ch)
 				{			
 					nStatesListCh.add(nStatesList.get(i));
@@ -355,7 +54,7 @@ public class Graph {
 	
 	public void MaxStId(){
 		int id = 0;
-		Set<Integer> allNodes = alfabett.keySet();	
+		Set<Integer> allNodes = DFA.alfabett.keySet();	
 		for (int innode : allNodes) {
 			if (innode > id) {
 				id = innode;
@@ -365,10 +64,10 @@ public class Graph {
 	}
 	
 	public void FirstFront() {
-		Set<Integer> allNodes = alfabett.keySet();	
+		Set<Integer> allNodes = DFA.alfabett.keySet();	
 		for (int innode : allNodes) {
-			if (innode != getFinalState() && innode != h) {
-				addOneInPares( getFinalState(), innode );				
+			if (innode != DFA.getFinalState() && innode != h) {
+				addOneInPares( DFA.getFinalState(), innode );				
 			}
 			if (innode != h ) {				
 				addOneInNewFront( h, innode );
@@ -445,10 +144,10 @@ public class Graph {
 	public void addOneWithoutRep(int in, int what) {
 		ArrayList<Integer> States = new ArrayList<Integer> ();		
 		int flag2 = 0;				
-		if(graph.containsKey(in))
+		if(DFA.graph.containsKey(in))
 		{			
-			States = graph.get(in);
-			graph.remove(in);				
+			States = DFA.graph.get(in);
+			DFA.graph.remove(in);				
 			for (int st : States) {
 				if( st == what ) {
 					flag2 = 1;
@@ -457,12 +156,12 @@ public class Graph {
 			if(flag2 == 0){
 				States.add(what);		
 			}				
-			graph.put(in, States);				
+			DFA.graph.put(in, States);				
 		}
 		else 
 		{		
 			States.add(what);
-			graph.put(in, States);					
+			DFA.graph.put(in, States);					
 		}		
 	}
 	
@@ -630,10 +329,10 @@ public class Graph {
 
 	
 	public void BuildRev() {		
-		Set<Integer> allNodes = alfabett.keySet();	
+		Set<Integer> allNodes = DFA.alfabett.keySet();	
 		for (int innode : allNodes) {
 			for (int fromnode : allNodes) {
-				ArrayList<Integer> Kids = getNextState(fromnode);
+				ArrayList<Integer> Kids = DFA.getNextState(fromnode);
 				if ( Kids != null ) {
 					for (Integer Kid : Kids) {
 						if (Kid == innode) {
@@ -655,9 +354,9 @@ public class Graph {
 				if ( keynode == h ) {
 					if( revgraph.get(valnode) != null ){
 						for (int node1 : revgraph.get(valnode)) {
-							for (int node2 : alfabett.keySet()) {
+							for (int node2 : DFA.alfabett.keySet()) {
 								flag = 0;
-								if(getNextSt(node2, alfabett.get(valnode).charAt(0)) != null) {
+								if(getNextSt(node2, DFA.alfabett.get(valnode).charAt(0)) != null) {
 									flag = 1;				
 								}	
 								if(flag == 0) {
@@ -669,7 +368,7 @@ public class Graph {
 						}
 					}
 				}
-				if (alfabett.get(keynode) == alfabett.get(valnode)) {
+				if (DFA.alfabett.get(keynode) == DFA.alfabett.get(valnode)) {
 					if( revgraph.get(keynode) != null && revgraph.get(valnode) != null ){
 						for (int node1 : revgraph.get(keynode)) {
 							for (int node2 : revgraph.get(valnode)) {
@@ -691,21 +390,21 @@ public class Graph {
 				continue;
 			}
 			for(int val : EqualPares.get(key)) {
-				for(int grkey : alfabett.keySet()) {
-					if(graph.get(grkey) != null) {
-						for(int grval : graph.get(grkey)) {
+				for(int grkey :DFA.alfabett.keySet()) {
+					if(DFA.graph.get(grkey) != null) {
+						for(int grval : DFA.graph.get(grkey)) {
 							if(grval == val) {
-								removeOne(grkey, grval);
+								DFA.removeOne(grkey, grval);
 								addOneWithoutRep(grkey,key);
 								break;
 							}
 						}
 					}	
 				}
-				for(int arrow : graph.get(val)) {
+				for(int arrow : DFA.graph.get(val)) {
 					addOneWithoutRep(key,arrow);
 				}
-				graph.remove(val);
+				DFA.graph.remove(val);
 			}			
 		}	
 	}
@@ -756,8 +455,8 @@ public class Graph {
 	
 	public void getEqualPares() {
 		ArrayList<Integer> States = new ArrayList<Integer> ();		
-		for(int key : alfabett.keySet() ) {
-			for(int val : alfabett.keySet() ) {
+		for(int key : DFA.alfabett.keySet() ) {
+			for(int val : DFA.alfabett.keySet() ) {
 				if(key != val) {
 					addInEqualPares(key, val);
 				}	
@@ -799,13 +498,13 @@ public class Graph {
 	}
 	
 	
-	public void minimizeDFA() {
+	public Graph minimizeDFA() {
 		/*for(int key : graph. keySet()) {
 			for(int val : graph.get(key)) {
 				System.out.print(key + "-" + val + "\n");
 			}	
 		}*/		
-		/*MaxStId();
+		MaxStId();
 		BuildRev();
 		FirstFront();
 		FrontResolve();
@@ -823,17 +522,17 @@ public class Graph {
 			}	
 		}
 		System.out.print("////////////////////////////////////" + "\n");
-		for(int key : graph. keySet()) {
-			for(int val : graph.get(key)) {
+		for(int key : DFA.graph. keySet()) {
+			for(int val : DFA.graph.get(key)) {
 				System.out.print(key + "-" + val + "\n");
 			}	
-		}		
-	}*/
+		}
+		return DFA;
+	}
 	
 	
 	//////////////////////////////////////////////////////////////////////////////////////
 	//////MINIMIZING//////MINIMIZING//////MINIMIZING//////MINIMIZING//////MINIMIZING//////
 	//////////////////////////////////////////////////////////////////////////////////////
-	
 
 }
