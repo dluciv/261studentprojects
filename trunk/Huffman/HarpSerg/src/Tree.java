1bd5c7c0e2950d79aaabd366b378c41f14ccf941
harpserg <harpserg@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-08-24 10:27:07 +0000

    что то напутал почищу и залью снова

--- trunk/Huffman/HarpSerg/src/Tree.java
+++
@@ -1,9 +0,0 @@
-class Tree {
-   public Tree lchild;       
-   public Tree rchild;
-   public boolean leaf;      
-   public int character;     
-   public long weight;
-   
-   public Tree() {}   
-}
