909c91f8eae260d10cc8a5d880a10254a49b45ac
nRitro <nRitro@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-09-18 08:33:47 +0000

    Hello W + comment

--- trunk/HelloWorld/Savenko/cHello.java
+++ trunk/HelloWorld/Savenko/cHello.java
@@ -1,9 +1,9 @@
+/* HelloWorld 
+** Savenko Maria ©2009*/
+
 
 public class cHello {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.print("Hello, World!!!");
 
