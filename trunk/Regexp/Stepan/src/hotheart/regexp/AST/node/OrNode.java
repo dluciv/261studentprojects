6fc9b08b709adc93c337b33a840a2635d3366422
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-09 08:10:42 +0000

    Nodes  now is different classes

---
+++ trunk/Regexp/Stepan/src/hotheart/regexp/AST/node/OrNode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hotheart.regexp.AST.node;
+
+/**
+ *
+ * @author m08ksa
+ */
+public class OrNode extends AbstractNode {
+    public AbstractNode left, right;
+    public OrNode(AbstractNode l, AbstractNode r)
+    {
+        left = l;
+        right = r;
+    }
+    @Override
+    public String toString() {
+        return "Or(" + left.toString() + ", " + right.toString() + ")";
+    }
+}
