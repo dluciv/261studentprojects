c64a9429ff7d0b75f2241a5af744500db68c3189
alekseev1989 <alekseev1989@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-15 17:46:33 +0000

    Add MailChecker

---
+++ trunk/MailChecker/AlekseevI/Main.java
@@ -0,0 +1,20 @@
+
+import emailchecker.EmailChacker;
+
+
+
+/**
+ *
+ * @author Lii
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+  System.out.println(EmailChacker.isEmail("mail@mail.org"));
+    }
+
+}
