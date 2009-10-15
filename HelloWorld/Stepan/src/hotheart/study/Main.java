ca946c6a106bf2c041d89583c8108579eecd3d60
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-15 15:41:20 +0000

    My Hello  World =/

---
+++ HelloWorld/Stepan/src/hotheart/study/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hotheart.study;
+
+/**
+ *
+ * @author HotHeart
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
