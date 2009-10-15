f3fe81c06087ca034b4935ad7bcd654aa1489774
lebedevdmt <lebedevdmt@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-15 19:03:32 +0000

    Add task Interface + exception reorganize Emailchecker

---
+++ interface/DmitryL/src/calculations/Addition.java
@@ -0,0 +1,12 @@
+//Lebedev Dmitry g261 2009 (c)
+
+package calculations;
+
+public class Addition implements Calculating {
+
+    public int calculate(int x, int y) {
+        return x + y;
+    }
+
+}
+
---
+++ interface/DmitryL/src/calculations/Calculating.java
@@ -0,0 +1,7 @@
+//Lebedev Dmitry g261 2009 (c)
+
+package calculations;
+
+public interface Calculating {
+    int calculate (int x, int y);
+}
