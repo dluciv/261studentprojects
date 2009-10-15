186ca8565f3fb4924a01a884c7cd52c4d29e786b
dmitry.42nd <dmitry.42nd@gmail.com>
2009-10-15 20:17:21 +0000

    + comments

--- Exception/Sdmitry/src/exception/Main.java
+++ Exception/Sdmitry/src/exception/Main.java
@@ -1,5 +1,12 @@
-package exception;
+/*
+ * the broken wheel of fortune
+ * soldatov dmity©, 2009
+ */
 
+package exception;
+/*
+ * The Wheel of Fortune is "suddenly" broken every time you wins =)
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
         WheelOfFortune.twistWheel();        
