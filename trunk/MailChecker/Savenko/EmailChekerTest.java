84b6e7ee818413e6b494eb00da3c55d0633987d5
nRitro <nRitro@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-16 07:30:31 +0000

    EmailChekerTest updated

--- trunk/MailChecker/Savenko/EmailChekerTest.java
+++ trunk/MailChecker/Savenko/EmailChekerTest.java
@@ -8,29 +8,66 @@ import org.junit.Test;
 
 public class EmailChekerTest {
 
-	@Test
-	public void testCheckTheEmailMatching() {
-		assertTrue(EmailChecker.checkTheEmailMatching("a@b.cc"));
-		assertTrue(EmailChecker.checkTheEmailMatching("yuri.gubanov@mail.ru"));
-		assertTrue(EmailChecker.checkTheEmailMatching("my@domain.info"));
-		assertTrue(EmailChecker.checkTheEmailMatching("_.1@mail.com"));
-		assertTrue(EmailChecker.checkTheEmailMatching("yurik@hermitage.museum"));
-         
-		assertTrue(EmailChecker.checkTheEmailMatching("a..b@mail.ru"));
-		assertTrue(EmailChecker.checkTheEmailMatching("a@b.c"));
-		assertTrue(EmailChecker.checkTheEmailMatching("yo@domain.domain"));
-		assertTrue(EmailChecker.checkTheEmailMatching(".a@mail.ru"));
-		assertTrue(EmailChecker.checkTheEmailMatching("1@mail.ru"));
-	}
+    @Test
+    public void testCheckTheEmailMatching1() {
+    assertTrue(EmailChecker.checkTheEmailMatching("a@b.cc"));
+    }
+    
+    public void testCheckTheEmailMatching2() {
+    assertTrue(EmailChecker.checkTheEmailMatching("yuri.gubanov@mail.ru"));
+    }
+    
+    public void testCheckTheEmailMatching3() {
+    assertTrue(EmailChecker.checkTheEmailMatching("my@domain.info"));
+    }
+    
+    public void testCheckTheEmailMatching4() {
+    assertTrue(EmailChecker.checkTheEmailMatching("_.1@mail.com"));
+    }
+    
+    public void testCheckTheEmailMatching5() {
+    assertTrue(EmailChecker.checkTheEmailMatching("yurik@hermitage.museum"));
+    }
+    
+    public void testCheckTheEmailFAILMatching1() {
+    assertTrue(EmailChecker.checkTheEmailMatching(".a@mail.ru"));
+    }
+    
+    public void testCheckTheEmailFAILMatching2() {
+    assertTrue(EmailChecker.checkTheEmailMatching("1@mail.ru"));
+    }
+    
+    public void testCheckTheEmailFAILMatching3() {
+    assertTrue(EmailChecker.checkTheEmailMatching("yo@domain.domain"));
+    }
+    
+    public void testCheckTheEmailFAILMatching4() {
+    assertTrue(EmailChecker.checkTheEmailMatching("a@b.c"));
+    }
+    
+    public void testCheckTheEmailFAILMatching5() {
+    assertTrue(EmailChecker.checkTheEmailMatching("a..b@mail.ru"));
+    }
 	
 	@Test
-	public void testCheckTheZipMatching(){
+	public void testCheckTheZipMatching1(){
 		assertTrue(EmailChecker.checkTheZipMatching("198504"));
-		assertTrue(EmailChecker.checkTheZipMatching("456780"));
-		assertTrue(EmailChecker.checkTheZipMatching("A 198504"));
-		assertTrue(EmailChecker.checkTheZipMatching("CA 198504"));
-		
-		assertTrue(EmailChecker.checkTheZipMatching("22"));
 	}
+    
+    public void testCheckTheZipMatching2(){
+    assertTrue(EmailChecker.checkTheZipMatching("456780"));
+    }
+    
+    public void testCheckTheZipMatching3(){
+    assertTrue(EmailChecker.checkTheZipMatching("A 198504"));
+    }
+    
+    public void testCheckTheZipMatching4(){
+    assertTrue(EmailChecker.checkTheZipMatching("CA 198504"));
+    }
+    
+    public void testCheckTheZipFAILMatching(){
+    assertTrue(EmailChecker.checkTheZipMatching("22"));
+    }
 
 }
