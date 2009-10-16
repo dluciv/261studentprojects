7400e71316820480f55dc4d46b7d5354dc2c805c
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-16 08:10:31 +0000

    EMailChecker refactoring

--- trunk/MailChecker/Stepan/src/hotheart/study/emailchecker/EMailChecker.java
+++ trunk/MailChecker/Stepan/src/hotheart/study/emailchecker/EMailChecker.java
@@ -1,22 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * EMailChecker class for Email checker program by Korshakov Stepan
  */
-
 package hotheart.study.emailchecker;
 
 import java.util.regex.Pattern;
 
 /**
- *
- * @author HotHeart
+ * @author Korshakov Stepan
  */
 public class EMailChecker {
-    
-    private static Pattern emailPattern = Pattern.compile("[a-zA-Z_]([a-zA-Z_0-9]*)(\\.[a-zA-Z0-9_]+)*@([a-zA-Z0-9-]+\\.)+(ru|cc|info|com|museum|su)");
 
-    public static boolean checkEMail(String data)
-    {
-         return emailPattern.matcher(data).matches();
+    private static final String login = "[a-zA-Z_]([a-zA-Z_0-9]*)(\\.[a-zA-Z0-9_]+)*";
+    private static final String domain = "([a-zA-Z0-9-]+\\.)+";
+    private static final String zone = "(([a-zA-Z]{2,4})|museum|travel)";
+    private static Pattern emailPattern = Pattern.compile(login + "@" + domain + zone);
+
+    public static boolean checkEmail(String data) {
+        return emailPattern.matcher(data).matches();
     }
 }
--- trunk/MailChecker/Stepan/src/hotheart/study/emailchecker/Main.java
+++ trunk/MailChecker/Stepan/src/hotheart/study/emailchecker/Main.java
@@ -1,13 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Main for Email checker by Korshakov Stepan
  */
-
 package hotheart.study.emailchecker;
 
+import java.util.Scanner;
+
 /**
- *
- * @author HotHeart
+ * @author Korshakov Stepan
  */
 public class Main {
 
@@ -15,7 +14,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        boolean a = EMailChecker.checkEMail("ex3ndr@inbox.ru");
-    }
+        Scanner in = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Write e-mail to ckeck(leave it blank to exit):");
+            String email = in.nextLine();
+            if (email.equals("")) {
+                System.out.println("Exiting...");
+                return;
+            } else {
+                if (EMailChecker.checkEmail(email)) {
+                    System.out.println(email + " is Valid e-mail");
+                } else {
+                    System.out.println(email + " is Invalid e-mail");
+                }
+            }
+        }
+    }
 }
