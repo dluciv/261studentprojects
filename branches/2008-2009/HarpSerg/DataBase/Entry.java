a051e48ef05372c3c4098c0989d7501a8cd6792d
HarpSerg <HarpSerg@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-22 16:17:31 +0000

    база данных на основе B-дерева

---
+++ branches/2008-2009/HarpSerg/DataBase/Entry.java
@@ -0,0 +1,60 @@
+
+
+/**
+ *
+ * @author HarpSerg
+ */
+import java.util.ArrayList;
+
+public class Entry {
+    private ArrayList<Integer> line_num = new ArrayList<Integer>();
+    static public final int SURNAME = 0,  TEL = 1;
+    private String sn;
+    private String tel;
+
+    Entry(String sn, String tel) {
+        this.sn = sn;
+        this.tel = tel;
+    }
+
+    Entry(String line, int num) {
+        int i = line.indexOf(' ');
+        sn = line.substring(0, i);
+        tel = line.substring(15);
+        line_num.add(num);
+    }
+    Entry(String line){
+        String[] args = line.split(":");
+        if(args.length == 1){
+            this.sn = args[0];
+            this.tel = args[0];
+        } else {
+            this.sn = args[0];
+            this.tel = args[1];
+        }
+    }
+
+    public ArrayList<Integer> getLineNums() {
+        return line_num;
+    }
+
+    public void addToLineNums(ArrayList<Integer> newNums) {
+        line_num.addAll(newNums);
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getSN() {
+        return sn;
+    }
+
+    public void setTel(String tel){
+        this.tel = tel;
+    }
+
+    public void setSN(String sn){
+        this.sn = sn;
+    }
+}
---
+++ branches/2008-2009/HarpSerg/DataBase/SingleKeyComp.java
@@ -0,0 +1,20 @@
+
+
+
+/**
+ *
+ * @author HarpSerg
+ */
+import java.util.Comparator;
+public class SingleKeyComp implements Comparator<Entry>{
+    private int field;
+    SingleKeyComp(int field){
+        this.field = field;
+    }
+    public int compare(Entry ent1, Entry ent2){
+        if(field == Entry.SURNAME)
+            return ent1.getSN().compareTo(ent2.getSN());
+        else
+            return ent1.getTel().compareTo(ent2.getTel());
+    }
+}
