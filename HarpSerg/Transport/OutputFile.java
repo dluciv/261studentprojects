6ca98ad1a3a91952dd21c77bd4f09db122d78188
HarpSerg <HarpSerg@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-22 16:20:07 +0000

    Программа для решения транспортной задачи линейного программирования с ограниченными пропускными способностями

---
+++ HarpSerg/Transport/OutputFile.java
@@ -0,0 +1,49 @@
+
+package transport;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+/**
+ *
+ * @author Artem
+ */
+
+public class OutputFile {
+
+	private FileOutputStream fileStream;
+
+	public OutputFile(String fileName) throws IOException
+	{
+		File file = new File(fileName);
+		if (file.exists())
+			file.delete();
+
+		if(file.createNewFile())
+			fileStream = new FileOutputStream(file);
+	}
+
+	public void write(byte[] data) throws IOException
+	{
+		fileStream.write(data);
+	}
+	public void write(byte data) throws IOException
+	{
+		fileStream.write(data);
+	}
+	public void write(int data) throws IOException
+	{
+		fileStream.write(data);
+	}
+	public void write(String data) throws IOException
+	{
+        for (int i = 0; i < data.length(); i++)
+            write((byte)data.charAt(i));
+	}
+
+	public void clean() throws IOException
+	{
+		this.fileStream.flush();
+		this.fileStream.close();
+	}
+
+}
