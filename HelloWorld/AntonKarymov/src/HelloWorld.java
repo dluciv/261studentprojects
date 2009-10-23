a67bec9bbe33d82c1fad3009eeae0ea5491243fc
AntonKarymov <AntonKarymov@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-23 12:38:56 +0000

    HelloWorld.java Ended Version

---
+++ HelloWorld/AntonKarymov/src/HelloWorld.java
@@ -0,0 +1,12 @@
+ /*
+ * Hello, Java!
+ * /Anton Karymov,g261,2009
+ */
+
+import java.lang.System;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
