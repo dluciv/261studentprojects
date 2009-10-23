97e79e98820cba2cea7fc02178b5b32a5d1cd063
nRitro <nRitro@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-23 08:22:36 +0000

    mmm

--- trunk/HelloWorld/Savenko/HelloWorld.java
+++ trunk/HelloWorld/Savenko/HelloWorld.java
@@ -6,8 +6,8 @@ package msavenko;
 import java.lang.System;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.print("Hello, World!!!");
-	}
+    public static void main(String[] args) {
+        System.out.print("Hello, World!!!");
+    }
 
 }
