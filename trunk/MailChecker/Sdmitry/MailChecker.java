04409b1b72f2665f480e1dae8c2f38dd8a76932f
dmitry.42nd@gmail.com <dmitry.42nd@gmail.com@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-23 08:42:25 +0000

    mailChecker with correct subDomain and correct style

--- trunk/MailChecker/Sdmitry/MailChecker.java
+++ trunk/MailChecker/Sdmitry/MailChecker.java
@@ -1,6 +1,6 @@
 /*
  *Program check is input string a mail
- *soldatov dmitry В©, 2009
+ *soldatov dmitry ©, 2009
  */
 
 package mailchecker;
@@ -8,30 +8,37 @@ import java.util.regex.*;
 import java.util.Scanner;
 
 public class MailChecker {
+    
+    private static String symbol = "[a-z0-9]";
+    private static String firstPart = "[a-z_]"; //login is divided into two 
+    //parts becouse one must begin with letter or "_"
+    private static String mainPart = "(" + symbol + "*|(\\.|_|-)" + symbol + ")*";
+    private static String login = firstPart + mainPart;
+    private static String subDomain = symbol + "(" + symbol + "*|(\\.)" + symbol + ")*";
+    private static String specialDomains = "(info)|(name)|(aero)|(arpa)|" +
+                                           "(coop)|(museum)|(mobi)|(travel)"; 
+    private static String topLevelDomain = "((" + symbol + "{2,3})|" + specialDomains + ")"; 
+    //supose that every 2-, 3-letter domain is true
+    private static String dot = ".";
+    private static String at = "@";
+    private static String mail = login + at + subDomain + dot + topLevelDomain;
+    private static Pattern mailRegexp = Pattern.compile(mail);
+    
     public static boolean isMail(String mail){
-        String symbol = "[a-z0-9]";
-        String firstpart = "[a-z_]"; //login is divided into two parts
-        //becouse one must begin with letter or "_"
-        String mainpart = "(" + symbol + "*|(\\.|_|-)" + symbol + ")*";
-        String domain2 = symbol + symbol + "*";
-        String domain1 = "((" + symbol + "{2,3})|" +
-                         "(info)|(name)|(aero)|(arpa)|" +
-                         "(coop)|(museum)|(mobi)|(travel))" ; //supose that
-        //every 2-, 3-letter domain is true
-        Pattern regexp = Pattern.compile(firstpart + mainpart + "@" + 
-                                         domain2 + "." + domain1);
-        Matcher m = regexp.matcher(mail);
-        return m.matches();
-              
+        
+        Matcher checker = mailRegexp.matcher(mail);
+        return checker.matches();
     }
 
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
         while (true){ //cycle close when line is empty
             System.out.println("Enter your mail addres: ");
             String mail = scan.nextLine();
-            if (mail.length() == 0) break;
+            if (mail.length() == 0) {
+                break;
+            }
             if (isMail(mail)) {
                 System.out.println("Your mail is correct!\n");
             }
