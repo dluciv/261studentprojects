f625abbf1e9e0fa82fea222f0599afb309e19583
nRitro <nRitro@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-23 08:23:12 +0000

    ~---~

--- trunk/Exception/Savenko/tryThrowingExceptionsTest.java
+++ trunk/Exception/Savenko/tryThrowingExceptionsTest.java
@@ -1,23 +1,26 @@
 /* JUnit test for Exception
  * By Savenko Maria (c)2009 */
 
+package msavenko;
+
+import java.lang.System;
 import org.junit.*;
 
 public class tryThrowingExceptionsTest {
 
-	@Test
-	public void testCheckTheClass1() {
-		TryThrowingExceptions.checkTheInterface(new AskForHelp());
-	}
+    @Test
+    public void testCheckTheClass1() {
+        TryThrowingExceptions.checkTheInterface(new AskForHelp());
+    }
 
-	@Test
-	public void testCheckTheClass2() {
-		TryThrowingExceptions.checkTheInterface(new AskName());
-	}
+    @Test
+    public void testCheckTheClass2() {
+        TryThrowingExceptions.checkTheInterface(new AskName());
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testCheckNullArgument() {
-		TryThrowingExceptions.checkTheInterface(null);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testCheckNullArgument() {
+        TryThrowingExceptions.checkTheInterface(null);
+    }
 
 }
