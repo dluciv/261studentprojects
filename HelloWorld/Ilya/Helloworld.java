ba409ef1ca20fa9f00589901ff7f8f14895fa47f
alekseev1989 <alekseev1989@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-10-26 18:50:13 +0000

    Add normal Helloworld

---
+++ HelloWorld/Ilya/Helloworld.java
@@ -0,0 +1,11 @@
+/* HelloWorld
+** Alekseev Ilya В©2009*/
+
+
+import java.lang.System;
+
+public class Helloworld {
+	public static void main (String[] args) {
+		System.out.println ("Hello, World!!!");
+	}
+}
