d1719d883660ad355ebeffd289c0f321fa34e094
dmitry.42nd <dmitry.42nd@gmail.com>
2009-10-27 12:12:19 +0000

    build.bat etc.

--- builder/Sdmitry/csharp/build.bat
+++ builder/Sdmitry/csharp/build.bat
@@ -1,2 +1,5 @@
-if exist hwpro (del /q hwpro) else (md hwpro)
-C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:hwpro/hw.exe hw.cs hwdll.cs
+set mPATH=w:\261studentprojects\Builder\Sdmitry\csharp
+if exist %mPATH%\hwpro (del /q hwpro) else (md %mPATH%\hwpro)
+cd %mPATH% 
+svn up
+C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:%mPATH%\hw.exe %mPATH%\hw.cs %mPATH%\hwdll.cs
--- builder/Sdmitry/csharp/hwdll.cs
+++ builder/Sdmitry/csharp/hwdll.cs
@@ -1,5 +1,3 @@
-namespace Hw {}
-
 public class Hwdll {
 	public String ck = "Hello, world!";
 }
