45c9db48862ff404da3f302c4131f2fe2446f4f7
dmitry.42nd <dmitry.42nd@gmail.com>
2009-10-30 09:49:01 +0000

    modified Exception

---
+++ Exception/Sdmitry/BrokenWheelException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exception;
+
+/**
+ *
+ * @author m08sdv
+ */
+public class BrokenWheelException extends Exception {
+    public BrokenWheelException(String message) {
+        super(message);
+    }
+}
+
