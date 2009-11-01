0b90c2e1e0593e3bfe3d2355ec895764073d5cf7
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-01 13:53:20 +0000

    Added Student Generation

--- ParentsAndStudens/Stepan/src/hotheart/parentsandstudens/Generator.java
+++ ParentsAndStudens/Stepan/src/hotheart/parentsandstudens/Generator.java
@@ -4,6 +4,7 @@
  */
 package hotheart.parentsandstudens;
 
+import com.sun.org.apache.bcel.internal.generic.GETFIELD;
 import java.util.Random;
 
 /**
@@ -13,21 +14,63 @@ import java.util.Random;
 public class Generator {
 
     private static final Random rnd = new Random();
-
-    private static final String[] namesMale = new String[]
-    {
+    private static final String[] namesMale = new String[]{
         "Александр", "Степан", "Дситрий", "Илья", "Сергей", "Михаил"
     };
-    
-    private static final String[] namesFemale = new String[]
-    {
+    private static final String[] namesFemale = new String[]{
         "Александра", "Алина", "Марина", "Анастасия", "Катерина", "Аня"
+    };
+    private static final String[] surnamesMale = new String[]{
+        "Коршаков", "Табуреткин", "Семенихин", "Ланцелотов", "Турбофишкин", "Орехов", "Белочкин"
+    };
+    private static final String[] faculties = new String[]{
+        "Математико-Механический", "Задолбайства и сельского хозяйства", "Юридический"
     };
 
     public static String generateName(Sex sex) {
-        if (sex == Sex.FEMALE)
+        if (sex == Sex.FEMALE) {
             return namesFemale[rnd.nextInt(namesFemale.length)];
-        else
+        } else {
             return namesMale[rnd.nextInt(namesMale.length)];
+        }
+    }
+
+    public static String generateSurname(Sex sex) {
+        if (sex == Sex.FEMALE) {
+            return surnamesMale[rnd.nextInt(surnamesMale.length)] + "а";
+        } else {
+            return surnamesMale[rnd.nextInt(surnamesMale.length)];
+        }
+    }
+
+    public static String generatePatronymic(String fatherName, Sex sex) {
+        if (sex == Sex.FEMALE) {
+            return fatherName + "овна";
+        } else {
+            return fatherName + "ович";
+        }
+    }
+
+    public static String generateFaculty() {
+        return faculties[rnd.nextInt(faculties.length)];
+    }
+
+    public static Student generateStudent(String fatherName, String surname) {
+        Sex sex = Sex.FEMALE;
+        if (rnd.nextBoolean()) {
+            sex = Sex.MALE;
+        }
+
+        String name = generateName(sex);
+        String patronymic = generatePatronymic(fatherName, sex);
+        String faculty = generateFaculty();
+
+        int age = 17 + rnd.nextInt(10);
+
+        if (rnd.nextBoolean()) {
+            return new Student(name, surname, patronymic, sex, age, faculty);
+        } else {
+            return new Botan(name, surname, patronymic, sex, age, faculty);
+        }
     }
 }
