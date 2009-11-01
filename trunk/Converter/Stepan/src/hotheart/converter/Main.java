e66b67364f12c218c671cd880e73f24fa7146268
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-01 11:09:23 +0000

    Initial Converter commit

---
+++ trunk/Converter/Stepan/src/hotheart/converter/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hotheart.converter;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HotHeart
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("HotHeart's IP converter");
+        mainFrame.setVisible(true);
+    }
+
+}
