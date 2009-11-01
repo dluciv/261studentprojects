766d57c86fe16e84b95cea067e70afd30fdf92c2
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-01 12:19:38 +0000

    Initial Commit

---
+++ trunk/ParentsAndStudens/Stepan/src/hotheart/parentsandstudens/Sex.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hotheart.parentsandstudens;
+
+/**
+ *
+ * @author HotHeart
+ */
+public enum Sex {
+    MALE, FEMALE
+}
