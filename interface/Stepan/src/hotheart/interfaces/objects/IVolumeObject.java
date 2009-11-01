6406b633574ee583d4702924ef22b6a8e3be0a88
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-01 10:54:45 +0000

    Added interfaces and classes. Added metod for calculating areas.

---
+++ interface/Stepan/src/hotheart/interfaces/objects/IVolumeObject.java
@@ -0,0 +1,14 @@
+/*
+ * Part of Interfaces home tast by Korshakov Stepan
+ * 261 Group
+ */
+
+package hotheart.interfaces.objects;
+
+/**
+ *
+ * @author Korshakov Stepan
+ */
+public interface IVolumeObject {
+    public float calcVolume();
+}
