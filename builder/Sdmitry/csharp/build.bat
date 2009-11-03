57852cdf1ff05b94d225419319b7723fb4f72889
dmitry.42nd <dmitry.42nd@gmail.com>
2009-11-03 10:12:17 +0000

    modif builder

--- builder/Sdmitry/csharp/build.bat
+++ builder/Sdmitry/csharp/build.bat
@@ -2,4 +2,4 @@ set mPATH=w:\261studentprojects\Builder\Sdmitry\csharp
 if exist %mPATH%\hwpro (del /q hwpro) else (md %mPATH%\hwpro)
 cd %mPATH% 
 svn up
-C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:%mPATH%\hw.exe %mPATH%\hw.cs %mPATH%\hwdll.cs
+C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:%mPATH%\hw.exe %mPATH%\hw.cs
