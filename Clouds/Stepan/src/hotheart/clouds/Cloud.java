291272d5621df402ab272b50e43cfc31fd94bf09
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-06 09:40:36 +0000

    Added IWind using to Cloud

--- Clouds/Stepan/src/hotheart/clouds/Cloud.java
+++ Clouds/Stepan/src/hotheart/clouds/Cloud.java
@@ -10,6 +10,13 @@ package hotheart.clouds;
  * @author m08ksa
  */
 public class Cloud {
+    
+    IWeather weather;
+    public Cloud(IWeather weather)
+    {
+        this.weather = weather;
+    }
+    
     public Creature getCreature() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
