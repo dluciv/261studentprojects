d350e5312945c3810041b6a9e3671dfe75f7f87c
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-06 09:48:11 +0000

    Some modifications

--- Clouds/Stepan/src/hotheart/clouds/Cloud.java
+++ Clouds/Stepan/src/hotheart/clouds/Cloud.java
@@ -2,21 +2,40 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package hotheart.clouds;
 
 /**
- *
  * @author m08ksa
  */
 public class Cloud {
     
+//    public static final TableRow[] creatures = new TableRow[]
+//    {
+//        new TableRow(true, 1, DayLightType.NIGHT, CreatureType.Bat)
+//    };
+
+    public class TableRow {
+
+        public boolean isLum;
+        public int wind;
+        public DayLightType daylight;
+        public CreatureType creatureType;
+        
+        public TableRow(boolean isLum, int wind, DayLightType daylight,
+                CreatureType creatureType) {
+            
+            this.isLum = isLum;
+            this.wind = wind;
+            this.daylight = daylight;
+            this.creatureType = creatureType;
+        }
+    }
     IWeather weather;
-    public Cloud(IWeather weather)
-    {
+
+    public Cloud(IWeather weather) {
         this.weather = weather;
     }
-    
+
     public Creature getCreature() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
---
+++ Clouds/Stepan/test/CloudTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+
+/**
+ *
+ * @author m08ksa
+ */
+public class CloudTest {
+    
+    @Test
+    public void testCloud()
+    {
+        
+    }
+}
