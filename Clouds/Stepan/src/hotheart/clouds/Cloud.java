b5e544b50bac8e24ba769cb478948ce5ccbef894
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-11 20:14:50 +0000

    Added Creature Creatinon

--- Clouds/Stepan/src/hotheart/clouds/Cloud.java
+++ Clouds/Stepan/src/hotheart/clouds/Cloud.java
@@ -8,28 +8,7 @@ package hotheart.clouds;
  * @author m08ksa
  */
 public class Cloud {
-    
-//    public static final TableRow[] creatures = new TableRow[]
-//    {
-//        new TableRow(true, 1, DayLightType.NIGHT, CreatureType.Bat)
-//    };
 
-    public class TableRow {
-
-        public boolean isLum;
-        public int wind;
-        public DayLightType daylight;
-        public CreatureType creatureType;
-        
-        public TableRow(boolean isLum, int wind, DayLightType daylight,
-                CreatureType creatureType) {
-            
-            this.isLum = isLum;
-            this.wind = wind;
-            this.daylight = daylight;
-            this.creatureType = creatureType;
-        }
-    }
     IWeather weather;
 
     public Cloud(IWeather weather) {
@@ -37,6 +16,58 @@ public class Cloud {
     }
 
     public Creature getCreature() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        IDayLight daylightPrivider = weather.getDayLight();
+        DayLightType daylight = daylightPrivider.getDayLightType();
+
+        IWind wind = weather.getWind();
+        int windSpeed = wind.getSpeed();
+
+        if (weather.getLuminary().isShining()) {
+            // isLuminary
+            if (daylight == DayLightType.NOON) {
+                if (windSpeed == 10) {
+                    return new Creature(CreatureType.Puppy);
+                } else {
+                    return null;
+                }
+            } else if (daylight == DayLightType.EVENING) {
+                if ((8 <= windSpeed) && (windSpeed <= 9)) {
+                    return new Creature(CreatureType.Kitten);
+                } else if ((4 <= windSpeed) && (windSpeed <= 7)) {
+                    return new Creature(CreatureType.Piglet);
+                } else {
+                    return null;
+                }
+            } else if (daylight == DayLightType.MORNING) {
+                if (windSpeed == 5) {
+                    return new Creature(CreatureType.Balloon);
+                } else {
+                    return null;
+                }
+            } else if (daylight == DayLightType.NIGHT) {
+                if ((1 <= windSpeed) && (windSpeed <= 3)) {
+                    return new Creature(CreatureType.Hedgehog);
+                } else {
+                    return null;
+                }
+            }
+        } else {
+            // not is Luminary
+
+            if (daylight == DayLightType.NIGHT) {
+                if ((1 <= windSpeed) && (windSpeed <= 3)) {
+                    return new Creature(CreatureType.Bearcub);
+                } else if (0 == windSpeed) {
+                    return new Creature(CreatureType.Bat);
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        return null;
     }
 }
