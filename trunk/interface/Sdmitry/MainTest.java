3722ee4e0c4c9b4019933a59e56dd75f590bc625
dmitry.42nd <dmitry.42nd@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-11-19 20:28:21 +0000

    + Tests

---
+++ trunk/interface/Sdmitry/MainTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacepkg;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @Test
+    public void testAnimalVoices_null_null() {
+        IAnimal justDog = null;
+        IAnimal justCat = null;
+        Main.animalVoices(justDog, justCat);
+    }
+
+    @Test
+    public void testAnimalVoices_null_Cat() {
+        IAnimal justDog = null;
+        IAnimal justCat = new Cat();
+        Main.animalVoices(justDog, justCat);
+    }
+
+    @Test
+    public void testAnimalVoices_Dog_Cat() {
+        System.out.println("animalVoices");
+        IAnimal justDog = new Dog();
+        IAnimal justCat = new Cat();
+        Main.animalVoices(justDog, justCat);
+    }
+
+    @Test
+    public void testAnimalVoices_Dog_null() {
+        System.out.println("animalVoices");
+        IAnimal justDog = new Dog();
+        IAnimal justCat = null;
+        Main.animalVoices(justDog, justCat);
+    }
+}
