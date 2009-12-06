6f5ebcac3949a28fcdb0f8027300d54d527b60a8
dmitry.42nd <dmitry.42nd@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-12-06 15:22:06 +0000

    generic & tests

---
+++ trunk/ParentsAndStudens/Sdmitry/IHuman.java
@@ -0,0 +1,19 @@
+/*
+ * Generic
+ * soldatov dmitry ©, 2009
+ */
+
+package generic;
+
+public interface IHuman {
+
+    public String getName();
+
+    public String getLastName();
+
+    public String getFatherName();
+
+    public Gender getGender();
+
+    public int getAge();
+}
