95941d478bea56e60b723b1b04104257114180f1
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-12-10 18:30:39 +0000

    [Editor]Finished prototype

--- trunk/editor/Stepan/Ex3Editor/MainWindow.xaml.cs
+++ trunk/editor/Stepan/Ex3Editor/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ex3Editor.Model;
 
 namespace Ex3Editor
 {
@@ -19,9 +20,78 @@ namespace Ex3Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        EditorEngine model = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            model = new EditorEngine();
+            this.MainTextBox.Text = model.Text;
+            this.MainTextBox.CaretIndex = model.State.CursorPos;
+            this.MainTextBox.ScrollToHorizontalOffset(model.State.HorizontalScroll);
+            this.MainTextBox.ScrollToVerticalOffset(model.State.VerticalScroll);
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (model.IsNew)
+            {
+                Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+                dialog.FileName = model.State.FileName;
+
+                bool? res = dialog.ShowDialog();
+                if (res.Value == true)
+                {
+                    model.State.FileName = dialog.FileName;
+                }
+                else
+                    return;
+            }
+
+            model.Text = MainTextBox.Text;
+            model.State.CursorPos = MainTextBox.CaretIndex;
+            model.State.HorizontalScroll = MainTextBox.HorizontalOffset;
+            model.State.VerticalScroll = MainTextBox.VerticalOffset;
+            model.SaveFile();
+            model.SaveState();
+            this.MainTextBox.Focus();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            bool? res = dialog.ShowDialog();
+            if (res.Value == true)
+            {
+                model = EditorEngine.LoadFile(dialog.FileName);
+                this.MainTextBox.Text = model.Text;
+            }
+            this.MainTextBox.Focus();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            if (!model.IsModified)
+            {
+                model.State.CursorPos = MainTextBox.CaretIndex;
+                model.State.HorizontalScroll = MainTextBox.HorizontalOffset;
+                model.State.VerticalScroll = MainTextBox.VerticalOffset;
+                model.SaveState();
+            }
+
+            App.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            model = EditorEngine.LoadState();
+            this.MainTextBox.Text = model.Text;
+            this.MainTextBox.CaretIndex = model.State.CursorPos;
+            this.MainTextBox.ScrollToHorizontalOffset(model.State.HorizontalScroll);
+            this.MainTextBox.ScrollToVerticalOffset(model.State.VerticalScroll);
+            this.MainTextBox.Focus();
         }
     }
 }
--- trunk/editor/Stepan/Ex3Editor/Properties/Settings.Designer.cs
+++ trunk/editor/Stepan/Ex3Editor/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Ex3Editor.Properties
-{
-
-
+namespace Ex3Editor.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "9.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public global::Ex3Editor.Model.EditorState LastState {
+            get {
+                return ((global::Ex3Editor.Model.EditorState)(this["LastState"]));
+            }
+            set {
+                this["LastState"] = value;
             }
         }
     }
