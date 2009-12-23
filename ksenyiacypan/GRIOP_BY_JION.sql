eedb0b730e0efe941a8b9baadbd92404ec3a5110
ksenyiacypan <ksenyiacypan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-12-23 20:52:01 +0000

    all without trigger

--- ksenyiacypan/GRIOP_BY_JION.sql
+++ ksenyiacypan/GRIOP_BY_JION.sql
@@ -1,1 +1,13 @@
-SELECT SEX, COUNT(*) FROM STUDENT GROUP BY SEX,
+-- оличество студентов и студенток
+SELECT SEX, COUNT(*) FROM STUDENT GROUP BY SEX;
+
+--Ќомера зачеток студентов со средним баллом большим, чем 4.5.
+SELECT zachetka, AVG(mark) 
+FROM students, student_exam 
+GROUP BY zachetka
+HAVING AVG(mark) > 4.5;
+
+--»мена отличников
+((STUDENT JOIN STUDENT_EXAM) WHERE MARK = '5'){NAME}
+
+
--- ksenyiacypan/select_udate.sql
+++ ksenyiacypan/select_udate.sql
@@ -1,4 +1,20 @@
-SELECT NAME FROM TEACHER;
+--Выдать всех студентов из первой группы
+SELECT * FROM STUDENT WHERE GROUP_ID = '1';
+
+--Все сведенья о студентах
+
 SELECT * FROM STUDENT;
+
+
+--Количество студентов
 SELECT COUNT(*) FROM STUDENT;
-UPDATE STUDENT SET ZACHETKS = ZACHETKA + 100;
+
+--Фамилии студентов и оценки
+SELECT NAME, MARK 
+FROM STUDENT, STUDENT_EXAM
+WHERE STUDENT.STUDENT_ID = STUDENT_EXAM.STUDENT_ID;
+
+--Поменять номер группы студента
+UPDATE STUDENT 
+SET GROUP_ID = '3'
+WHERE NAME = 'Цыпан Ксения' ;
