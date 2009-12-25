340640337e7adfbd263750174f73abfc4d8a2a34
alekseev1989 <alekseev1989@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-12-25 18:39:27 +0000

    add exeption

---
+++ trunk/Exception/AlekseevI/src/exception/Calculator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Lii
+ */
+public interface Calculator {
+
+    int calculate(int firstNumber, int secondNumber);
+}
