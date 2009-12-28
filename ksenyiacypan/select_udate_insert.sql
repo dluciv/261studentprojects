7b18ebd46d9573d28a9e50d8318dc23f8018cceb
ksenyiacypan <ksenyiacypan@27a565e4-e655-0410-a04c-dd7a8689bede>
2009-12-28 18:10:53 +0000

    insert, update, join

---
+++ ksenyiacypan/select_udate_insert.sql
@@ -0,0 +1,34 @@
+--Выдать всех студентов из первой группы
+SELECT * FROM STUDENT WHERE GROUP_ID = '1';
+
+--Все сведенья о студентах
+
+SELECT * FROM STUDENT;
+
+
+--Количество студентов
+SELECT COUNT(*) FROM STUDENT;
+
+--Фамилии студентов и оценки
+SELECT NAME, MARK 
+FROM STUDENT, STUDENT_EXAM
+WHERE STUDENT.STUDENT_ID = STUDENT_EXAM.STUDENT_ID;
+
+--Поменять номер группы студента
+UPDATE STUDENT 
+SET GROUP_ID = '3'
+WHERE NAME = 'Цыпан Ксения' ;
+
+insert into STUDENT(name, za4etka_number, group_id, sex) values ('Хисмутова Анастасия', '010', 1, 'женский');
+insert into STUDENT(name, za4etka_number, group_id, sex) values ('Максимова Анастсия', '011', 2, 'женский');
+insert into STUDENT(name, za4etka_number, group_id, sex) values ('Савва Олег', '012', 3, 'мужской');
+
+
+
+UPDATE SESSION 
+SET DATE = 12.01.10
+WHERE SUBJECT_ID = 'Математический анализ' ;
+
+
+insert into TEACHER(name, subject_id) values ('Касьянова Нататья Петровна', 5);
+insert into TEACHER(name, subject_id) values ('Ефименко Георгий Романович', 2);
