b857710ebe69d7f7dd418e74d22e697ca8ed947f
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-04-15 14:25:55 +0000

    Добавил константу неизвестного символа в InfiniteTape

--- trunk/Automaton Theory/Stepan/src/name/stepa/turing/InfiniteTape.java
+++ trunk/Automaton Theory/Stepan/src/name/stepa/turing/InfiniteTape.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
  * Autor: Korshakov Stepan (korshakov.stepan@gmail.com)
  */
 public class InfiniteTape {
+    public final static char UNKNOWN_VALUE = '\0';
+
     private HashMap<Integer, Character> map = new HashMap();
 
     public void set(Integer index, Character value) {
@@ -16,7 +18,7 @@ public class InfiniteTape {
 
     public Character get(Integer index) {
         if (!map.containsKey(index))
-            return '\0';
+            return UNKNOWN_VALUE;
         else
             return map.get(index);
     }
