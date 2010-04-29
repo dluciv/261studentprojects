a4f30e5b3a7e78f241208c3d22b3b7b92f5ab202
yaskovsergey <yaskovsergey@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-04-29 08:51:18 +0000

    создание компилятора

--- trunk/Interpreter (with let, if, GUI etc)/KarymovLebedevYaskov/Compiler/src/AST/Tree.java
+++ trunk/Interpreter (with let, if, GUI etc)/KarymovLebedevYaskov/Compiler/src/AST/Tree.java
@@ -1,9 +1,8 @@
 //Lebedev Dmitry 2010 (c)
 package AST;
 
-public interface Tree {
-
-    public int evaluate();
-
-    public void print();
+public abstract class Tree {
+//    public int evaluate();
+//
+//    public void print();
 }
