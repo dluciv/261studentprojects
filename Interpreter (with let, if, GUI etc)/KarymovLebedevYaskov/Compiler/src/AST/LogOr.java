6e808fa6aace4dd2dd45a7ae7d8b3b9607c9adc0
yaskovsergey <yaskovsergey@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-05-02 12:09:24 +0000

    вариант дерева-2

---
+++ Interpreter (with let, if, GUI etc)/KarymovLebedevYaskov/Compiler/src/AST/LogOr.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package AST;
+
+public class LogOr extends BinaryOperation {
+    public LogOr(LogOperand left, LogOperand right) {
+        super(left, right);
+    }
+}
