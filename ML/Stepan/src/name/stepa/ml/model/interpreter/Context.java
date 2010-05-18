2bfc3581c654e58b2fcde30cd52a976c35e8b622
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-05-18 08:39:59 +0000

    Добавил отображение списка текущих переменных.

--- ML/Stepan/src/name/stepa/ml/model/interpreter/Context.java
+++ ML/Stepan/src/name/stepa/ml/model/interpreter/Context.java
@@ -6,6 +6,7 @@ import java.util.TreeMap;
  * Autor: Korshakov Stepan (korshakov.stepan@gmail.com)
  */
 public class Context {
+    public TreeMap<String, Object> values;
 
     public Context() {
         this.values = new TreeMap<String, Object>();
@@ -15,17 +16,16 @@ public class Context {
         this.values = values;
     }
 
-    private TreeMap<String, Object> values;
-
     public Object put(String name, Object value) {
         return values.put(name, value);
     }
 
     public Object get(String name) {
-        if (values.containsKey(name))
+        if (values.containsKey(name)) {
             return values.get(name);
-        else
+        } else {
             return null;
+        }
     }
 
     public Context clone() {
