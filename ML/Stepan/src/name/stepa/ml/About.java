b8e986b0cc9dc68d1f7b3af41d5d3ae645d1813e
korshakov.stepan <korshakov.stepan@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-05-30 21:52:11 +0000

    Немного улучшил About

--- ML/Stepan/src/name/stepa/ml/About.java
+++ ML/Stepan/src/name/stepa/ml/About.java
@@ -1,19 +1,25 @@
 package name.stepa.ml;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class About extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
+    private JPanel panel1;
 
     public About() {
         setContentPane(contentPane);
         pack();
         setModal(false);
-        setSize(300, 100);
+        //setSize(300, 100);
+        setResizable(false);
         getRootPane().setDefaultButton(buttonOK);
+        Toolkit toolkit = getToolkit();
+        Dimension size = toolkit.getScreenSize();
+        setLocation((size.width - getWidth()) / 2, (size.height - getHeight()) / 2);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
