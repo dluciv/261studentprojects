385dee40c748b5b94cde88cfb9a2701c710ebbf3
yaskovsergey <yaskovsergey@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-06-03 07:24:43 +0000

    добавлены тесты для интерпретатора; добавлена презентация

--- Interpreter (with let, if, GUI etc)/Compiler/src/tools/Tool.java
+++ Interpreter (with let, if, GUI etc)/Compiler/src/tools/Tool.java
@@ -9,6 +9,7 @@ public class Tool {
     private static LinkedList<LogChangedListener> listeners = new LinkedList<LogChangedListener>();
 
     public static void fixError(String message, Position position) {
+        //System.out.println("dsfsdfs");
         errorQnt++;
         errorLog.add(new ErrorLogCell(message, position));
         for (LogChangedListener listener : listeners) {
@@ -21,6 +22,7 @@ public class Tool {
     }
 
     public static int getErrorQnt() {
+        //System.out.println("dsfsdfs");
         return errorQnt;
     }
 
