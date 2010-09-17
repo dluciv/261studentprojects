b77d28ce94ae4f4106617885d84dec82693c16ea
eketerinavolkova <eketerinavolkova@27a565e4-e655-0410-a04c-dd7a8689bede>
2010-09-17 09:35:32 +0000

    files moved

---
+++ trunk/AST&RPN Builder/Volkova/ide/Lexeme.java
@@ -0,0 +1,17 @@
+package ide;
+
+public enum Lexeme {
+
+    Plus, Minus, Div, Mult, LeftBracket, RightBracket,
+    EOL, And, Or, Not, Assignment, True, False,
+    Equal, UnEq, Less, Greater, LessEq, GreaterEq,
+    LBr, RBr, UnKnown, Semi,
+    If, Then, Else, Let, In, Print, Begin, End, Fun, OpFun,
+    Number, Id, Rec;
+}
+
+
+   
+
+
+   
